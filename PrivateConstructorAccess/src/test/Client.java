c0ea4f9eba365aceb3f4fde0d7b49adb4740ad97
Subramanyam Shreenivas <37250066+subbu-k@users.noreply.github.com>
2018-04-08 22:12:49 -0700

    Update Client.java

--- PrivateConstructorAccess/src/test/Client.java
+++ PrivateConstructorAccess/src/test/Client.java
@@ -6,7 +6,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		try {
-		Class c =	Class.forName("beans.Test");
+		Class c = Class.forName("beans.Test");
 			Constructor con[] =c.getDeclaredConstructors();
 			con[0].setAccessible(true);
 			con[0].newInstance(null);
