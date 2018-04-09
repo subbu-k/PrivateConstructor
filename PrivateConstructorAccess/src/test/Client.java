093ccab46f73dd2f273591174d54fad3c8c45edc
subbu <subbu@personal>
2018-04-08 22:08:02 -0700

    Private Constructor Access

---
+++ PrivateConstructorAccess/src/test/Client.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.lang.reflect.Constructor;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+		Class c =	Class.forName("beans.Test");
+			Constructor con[] =c.getDeclaredConstructors();
+			con[0].setAccessible(true);
+			con[0].newInstance(null);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
